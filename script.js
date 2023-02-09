0ed7c4d1ee4c1352125c914831e05f74cf2da83b
Nathan <Bowers.Nathanx@gmail.com>
2023-02-09 14:42:05 -0500

    make grid and grid effects when mouseenters

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container');
+let gridSize = 256//prompt("size");
+
+function makeGrid(gridSize){
+ for (let i = 0; i < gridSize; i++) {
+    let grid= document.createElement('div')
+    grid.classList.add('grid');
+    container.appendChild(grid)
+}}
+makeGrid(gridSize)
+let grid =document.querySelectorAll('.grid')
+grid.forEach(grid => {
+    grid.addEventListener('mousemove', draw)
+function draw(e){
+    e.target.classList.add('red');   
+}})
+
